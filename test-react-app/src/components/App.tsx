38fc3a6c5645267244a00a86977a8e0d8b7f0db5
jungpaeng <meis1541@naver.com>
2021-07-06 01:48:58 +0900

    feat: wrapPromise
    from react suspense example

--- test-react-app/src/components/App.tsx
+++ test-react-app/src/components/App.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
 
+function wrapPromise(promise: Promise<any>) {
+  let status = "pending";
+  let result: any;
+
+  const suspender = promise.then(
+    (r) => {
+      status = "success";
+      result = r;
+    },
+    (e) => {
+      status = "error";
+      result = e;
+    }
+  )
+
+  return {
+    read() {
+      if (status === "pending")
+        throw suspender;
+      if (status === "error")
+        throw result;
+
+      return result;
+    }
+  }
+}
+
+function fakeAPI() {
+  return new Promise(resolve => setTimeout(() => {
+    resolve("SUCCESS");
+  }, 1000));
+}
+function callFakeAPI() {
+  const apiResult = fakeAPI()
+  return wrapPromise(apiResult);
+}
+
+const initValue = callFakeAPI();
+
 function App() {
   return (
-    <div className="App">
-      HELLO, WORLD
-    </div>
+    <React.Suspense fallback={<p>Loading...</p>}>
+      <Child value={initValue}/>
+    </React.Suspense>
   );
 }
 
 export default App;
+
+function Child({value}: any) {
+  return <p>{value.read()}</p>
+}
